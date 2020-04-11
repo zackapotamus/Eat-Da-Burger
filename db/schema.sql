87c772718118d5169d5cd4df119573271e64558e
Zack Hayden <signbarn@gmail.com>
2020-04-10 22:20:40 -0400

    jawsdb connection stuff

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 ---### Schema
-DELETE DATABASE IF EXISTS burger_db;
+DROP DATABASE IF EXISTS burger_db;
 CREATE DATABASE burger_db;
 USE burger_db;
 
@@ -8,5 +8,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id)
 );
